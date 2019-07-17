0a3823a0ca30690e3246e115394f12855e1fa0cc
Arnon <arnons1@users.noreply.github.com>
2019-07-17 14:52:59 +0200

    Update pysqream.py

--- pysqream/pysqream.py
+++ pysqream/pysqream.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-PYSQREAM_VERSION = "2.1.4"
+PYSQREAM_VERSION = "2.1.4a1"
 """
 Python2.7/3.x connector for SQream DB
 
