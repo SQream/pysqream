1ddcaf9f5774cf76b9abc7df5cc45eb606890ff2
Arnon Shimoni <arnon@sqream.com>
2019-12-30 23:18:21 +0100

    Re-add Python 3.7 support, fix setup.py to compile

--- setup.py
+++ setup.py
@@ -9,31 +9,28 @@ setup_params = dict(
     name =          'pysqream',
     version =       '3.0.0',
     description =   'DB-API connector for SQreamDB', 
-    long_description=long_description,
-    url="https://github.com/SQream/pysqream",
-    author="SQream Technologies",
-    author_email="info@sqream.com",
-    packages =  ['pysqream'], 
-    classifiers=[
+    long_description = long_description,
+    url = "https://github.com/SQream/pysqream",
+    author = "SQream Technologies",
+    author_email = "info@sqream.com",
+    packages = ['pysqream'], 
+    classifiers = [
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    keywords='database db-api sqream sqreamdb',
-    python_requires='>=3.8'
+    keywords = 'database db-api sqream sqreamdb',
+    python_requires = '>=3.7.1'
+)
 
-    '''
+'''
     # install_requires=['sqlalchemy'],
     # package_dir = {'': 'pysqream'},
     entry_points={
         'sqlalchemy.dialects':
             ['sqream = pysqream.dialect:SqreamDialect']
     },
-    # sqream://sqream:sqream@localhost/master
-    # sqream+sqream_dialect://sqream:sqream@localhost/master
-    '''
-)
-
+'''
 
 
 if __name__ == '__main__':
