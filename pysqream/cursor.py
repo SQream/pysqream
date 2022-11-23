70bc8fcc2ee3c986ab5bd869e4bceb898ac30017
danielg <danielg@sqreamtech.com>
2022-11-23 12:26:53 +0200

    remoe del

--- pysqream/cursor.py
+++ pysqream/cursor.py
@@ -35,13 +35,13 @@ class Cursor:
         self.rows_per_flush = 0
         self.lastrowid = None
 
-    def __del__(self):
-        try:
-            logger.debug("Try to destroy open cursor")
-            self.close()
-        except Exception as e:
-            # if "Trying to close a connection that's already closed" not in repr(e):
-            log_and_raise(ProgrammingError, e)
+    # def __del__(self):
+    #     try:
+    #         logger.debug("Try to destroy open cursor")
+    #         self.close()
+    #     except Exception as e:
+    #         # if "Trying to close a connection that's already closed" not in repr(e):
+    #         log_and_raise(ProgrammingError, e)
 
     def get_statement_type(self):
 
