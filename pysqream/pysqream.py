354bead6b9c8b6d50478e580248c2f8a8f0baf85
danielg <danielg@sqreamtech.com>
2022-12-14 20:04:06 +0200

    fix more import

--- pysqream/pysqream.py
+++ pysqream/pysqream.py
@@ -3,7 +3,7 @@
 
 from datetime import datetime, date, time as t
 import time
-from globals import __version__
+from pysqream.globals import __version__
 from pysqream.logger import log_and_raise, start_logging, stop_logging
 from pysqream.connection import Connection
 
