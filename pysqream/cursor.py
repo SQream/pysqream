697f771162b0572c6271a278dfa9996faae1b34f
danielg <danielg@sqreamtech.com>
2023-01-09 09:29:40 +0200

    end ping pong after execute

--- pysqream/cursor.py
+++ pysqream/cursor.py
@@ -120,6 +120,9 @@ class Cursor:
         self.parsed_rows = []
         self.parsed_row_amount = 0
 
+        print(f'Executing statement over connection {self.conn.connection_id} with statement id {self.stmt_id}:\n{stmt}')
+        if self.ping_loop is not None:
+            _end_ping_loop(self.ping_loop)
         if logger.isEnabledFor(logging.INFO):
             logger.info \
                 (f'Executing statement over connection {self.conn.connection_id} with statement id {self.stmt_id}:\n{stmt}')
@@ -478,7 +481,7 @@ class Cursor:
         self.closed = True
         self.conn.close_connection()
         self.buffer.close()
-        _end_ping_loop(self.ping_loop)
+        # _end_ping_loop(self.ping_loop)
 
     def __enter__(self):
         return self
