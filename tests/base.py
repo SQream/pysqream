cb20e6bcd7f96c09e75adae7c4a50d3466038ba3
danielg <danielg@sqreamtech.com>
2023-01-09 17:42:21 +0200

    change import on base

--- tests/base.py
+++ tests/base.py
@@ -4,13 +4,13 @@ import sys
 import os
 sys.path.append(os.path.abspath(__file__).rsplit('tests/', 1)[0] + '/tests/')
 sys.path.append(os.path.abspath(__file__).rsplit('tests/', 1)[0] + 'pysqream/')
-from pysqream.pysqream import connect
+import pysqream
 from tests.pytest_logger import Logger
 
 
 def connect_dbapi(ip, clustered=False, use_ssl=False):
     port = (3109 if use_ssl else 3108) if clustered else (5001 if use_ssl else 5000)
-    return connect(ip, port, 'master', 'sqream', 'sqream', clustered, use_ssl)
+    return pysqream.connect(ip, port, 'master', 'sqream', 'sqream', clustered, use_ssl)
 
 
 class TestBase():
