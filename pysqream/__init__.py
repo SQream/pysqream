a8a96f47201c4835be54443739c0f66decf60f61
Andrey Dybov <andreyd@sqreamtech.com>
2023-06-21 10:50:24 +0200

    Fix path interference issue between pysqream and pysqream_blue
    This commit addresses the problem of interference between the pysqream
    and pysqream_blue folders when both were added to the PATH.
    The interference occurred in cases where absolute imports were used,
    leading to conflicts and confusion.
    
    Previously, the pysqream.connection module had the line from ping
    import PingLoop, while the pysqream_blue module also had a ping module
    with a PingLoop (pretend they are different). Both folders were added
    to the PATH, making them visible to Python during module imports.
    
    As a result, when importing PingLoop from pysqream.connection, it
    inadvertently imported PingLoop from pysqream_blue.ping due to the
    presence of the pysqream_blue folder in the PATH. This example
    illustrates a simple case, but the issue extended to other modules and
    caused confusion.
    
    This commit removes the inclusion of the whole folder of pysqream in
    the PATH, resolving the interference problem between pysqream and
    pysqream_blue at pysqream side.
    
    After that all absolute import should be replaced in package by
    relative imports

--- pysqream/__init__.py
+++ pysqream/__init__.py
@@ -1,3 +1,23 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+"""Python connector / adapter for SQream DB
+
+The Python connector for SQream DB is a Python DB API 2.0-compliant
+interface for developing Python applications with SQream DB.
+
+The SQream Python connector provides an interface for creating and
+running Python applications that can connect to a SQream DB database.
+It provides a lighter-weight alternative to working through native C++
+or Java bindings, including JDBC and ODBC drivers.
+"""
+# Avoid adding the entire folder to sys.path.
+# Importing modules directly in files can be confusing.
+# For example, in connection.py, instead of using "from ping import ...",
+# use a relative import like this:
+# from .ping import ...
 from .pysqream import connect, __version__, enable_logs, stop_logs
+
+__all__ = [
+    "connect",
+    "__version__",
+    "enable_logs",
+    "stop_logs",
+]
