1f770b9ccae9127d9bd910c74bdfbbaf9e964039
Michael Rogozin <michaelr@sqreamtech.com>
2024-09-30 23:33:26 +0300

    fix: remove `ParametrizedStatementException` as legacy

--- pysqream/utils.py
+++ pysqream/utils.py
@@ -110,6 +110,3 @@ class NotSupportedError(DatabaseError):
 
 class ArraysAreDisabled(DatabaseError):
     pass
-
-class ParametrizedStatementError(DatabaseError):
-    """Exception for parametrized statements, for example when parameters amount greater than placeholders amount"""
